e5fb960414e80adc571eca005b6206539eecdc29
dakshgupta2002 <dakshgupta2002@gmail.com>
2022-11-18 15:22:04 +0530

    fallback mechanism routing

--- web-front-end/src/ProtectedRoute.js
+++ web-front-end/src/ProtectedRoute.js
@@ -1,19 +1,18 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { auth } from './firebase';
-
 export default function ProtectedRoute(props) {
   const [user, setUser] = useState(false);
-  useEffect( () => {
+  useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      if (user){
+      if (user) {
         setUser(true);
-      }else setUser(false);
+      } else setUser(false);
     })
   })
 
   if (user) return props.children;
-  else return <>
+  else return <div className='d-flex flex-column align-content-center justify-content-center vw-100 vh-100'>
     <h1>You are not logged in</h1>
-    <button href="/login">Login</button>
-  </>
+    <a className="btn btn-primary" href="./login" role="button">Login</a>
+  </div>
 }
